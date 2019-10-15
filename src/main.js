7077dbeb501092824fb5bdd2b4be082fbe5f1df4
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-10-15 16:16:15 -0300

    comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// Step Zero: no restrictions.
-
 function area(shape) {
     switch (shape.kind) {
         case 'circle': return Math.PI * Math.pow(shape.radius, 2);
@@ -14,26 +12,58 @@ function displayArea(shape) {
     console.log(`area of ${shape.kind}: ${rounded}`);
 }
 
-// valid invocations
+/* 
+    Valid invocations 
+*/
 displayArea({ kind: 'circle', radius: 2 });
 displayArea({ kind: 'square', side: 10 });
 displayArea({ kind: 'rectangle', height: 20, width: 10 });
 displayArea({ kind: 'circle', radius: 2_000 });
 
-// invalid invocations #1
+/* 
+    Invalid invocations - trivial
+*/
 displayArea();
-displayArea(1, 1, 1, 1); // up to 65.535 on chrome.
-displayArea(1, 2, 3, 4);
+displayArea(1, 1, 1, 1);
+displayArea(1, 2, 3, 4, 1, 2, 3, 4, 5, "is there a limit?");
 
-// invalid invocations #2
+/* 
+    Invalid invocations - easy
+*/
 displayArea(displayArea);
 displayArea(1);
 displayArea({ kind: 'rectangle', width: 10, heigth: 20 });
 
-// invalid invocations #3
+/* 
+    Invalid invocations - hard
+*/
+displayArea({ kind: 'cicrle', radius: 10 });
+displayArea({ kind: 'triangle', height: 10, width: 15 });
+
+/*
+    Invalid invocations - hard
+*/
 displayArea({ kind: 'rectangle', side: 10, height: 20 });
 displayArea({ kind: 'square', height: 10, width: 10 });
 
-// invalid invocations #4
-displayArea({ kind: 'cicrle', radius: 10 });
-displayArea({ kind: 'triangle', height: 10, width: 15 });
+/*
+    Extra - Support 'triangle' shapes
+*/
+
+// displayArea({ kind: 'triangle', height: 10, width: 15 });
+
+
+/*
+    Extra 2:
+
+    Fun fact A: how many parameters can you add?
+    - Platform specific: up to 65.535 on Chrome
+
+    Fun fact B:
+    - given a function "action" that accepts a boolean parameter
+    - it's domain is [false, true], 2 possible invocations: action(false) / action(true)
+    - if we limit ourselves to 270, in javascript, there are 2^270 possible invocations (only using true and false)
+    - that's a bigger than 10^80
+    - 10^80 is the number of atoms on the universe
+    - every single javascript function has a range *way* bigger than the number of atoms in the universe
+*/
